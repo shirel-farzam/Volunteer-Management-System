dd47f031bebd90d71197e30d99c444e9f8f9500a
shire <shirelf15gmail.com>
2024-11-04 14:05:00 +0200

    Initial Code + Welcome functions

---
+++ dotNet5785_-4642_7701/stage0/Program4642.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+partial class Program4642
+{
+    private static void Main(string[] args)
+    {
+        welcome4642();
+        Console.ReadKey();
+    }
+
+    static partial void welcome7701();
+    private static void welcome4642()
+    {
+        Console.WriteLine("Enter your name ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console", name);
+    }
+}
