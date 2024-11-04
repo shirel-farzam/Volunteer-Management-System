0bdbf850f12c804a435e1728d3e31f16403cca05
איילת בניסטי <ayelet.benisty@gmail.com>
2024-11-04 15:37:55 +0200

    j

--- dotNet5785_-4642_7701/stage0/Program4642.cs
+++ dotNet5785_-4642_7701/stage0/Program4642.cs
@@ -4,14 +4,14 @@ partial class Program4642
 {
     private static void Main(string[] args)
     {
-        welcome4642();
+        welcome7701();
         Console.ReadKey();
     }
 
     static partial void welcome7701();
     private static void welcome4642()
     {
-        Console.WriteLine("Enter your name ");
+        Console.WriteLine("Enter your name: ");
         string name = Console.ReadLine();
         Console.WriteLine("{0}, welcome to my first console", name);
     }
---
+++ dotNet5785_-4642_7701/stage0/Program7701.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program7701
+    {
+        static partial void welcome7701()
+        {
+            Console.WriteLine("I am also here!!");
+        }
+    }
+}
