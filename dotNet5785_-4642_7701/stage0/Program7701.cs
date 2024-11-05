13c69e633a8faa25912081b13f9a9c4207634692
איילת בניסטי <ayelet.benisty@gmail.com>
2024-11-05 14:19:34 +0200

    m

--- dotNet5785_-4642_7701/stage0/Program7701.cs
+++ dotNet5785_-4642_7701/stage0/Program7701.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace stage0
 {
-    internal partial class program
+    partial class program
     { 
         static partial void welcome7701()
         {
