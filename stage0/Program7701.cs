e98e0a51c20fffa640cba73449db9dda96772e00
Ayelet929 <ayelet.benisty@gmail.com>
2024-11-11 16:27:29 +0200

    Add files via upload

---
+++ stage0/Program7701.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace stage0
+{
+    partial class program
+    {
+        static partial void welcome7701()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
