56529035a251bf9cd530fb8281adb3457637fbc1
איילת בניסטי <ayelet.benisty@gmail.com>
2024-11-12 21:27:51 +0200

    מעודכן

--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -1,36 +1,81 @@
 ﻿namespace Dal;
-using DalApi;
 using DO;
+using DalApi;
+using System;
 
-public class VolunteerImplementation : IVolunteer
+
+public class VolunteerlImplementation : IVolunteer
 {
+
     public void Create(Volunteer item)
     {
-        throw new NotImplementedException();
+
+
+        // בדיקה אם קיים אובייקט עם אותו מזהה
+        if (DataSource.Volunteers.Any(v => v.Id == item.Id))
+        {
+            throw new Exception($"Volenteer with ID={item.Id} already exists"); ;
+        }
+
+
+
+        DataSource.Volunteers.Add(item);
+
+        // return item.Id;
     }
 
-    public void Delete(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteAll()
-    {
-        throw new NotImplementedException();
-    }
 
     public Volunteer? Read(int id)
     {
-        throw new NotImplementedException();
+        var volunteer1 = DataSource.Volunteers.FirstOrDefault(v => v.Id == id);
+        if (volunteer1 == null)
+            return null;
+        return volunteer1;
     }
 
     public List<Volunteer> ReadAll()
     {
-        throw new NotImplementedException();
+
+
+
+        return new List<Volunteer>(DataSource.Volunteers);
     }
 
     public void Update(Volunteer item)
     {
-        throw new NotImplementedException();
+
+
+        var index = DataSource.Volunteers.FindIndex(v => v.Id == item.Id);
+        if (index == -1) throw new Exception($"Volunteer with ID={item.Id} not exists");
+
+        DataSource.Volunteers[index] = new Volunteer
+        {
+            Id = item.Id,
+            FullName = item.FullName,
+            PhoneNumber = item.PhoneNumber,
+            Email = item.Email,
+            TypeDistance = item.TypeDistance,
+            Job = item.Job,
+            Active = item.Active,
+            Password = item.Password,
+            FullAddress = item.FullAddress,
+            Latitude = item.Latitude,
+            Longitude = item.Longitude,
+            MaxReading = item.MaxReading,
+
+        };
+    }
+
+    public void Delete(int id)
+    {
+        var volunteer = DataSource.Volunteers.FirstOrDefault(v => v.Id == id);
+        if (volunteer == null) throw new Exception($"Volunteer with ID={id} not exists");
+
+        DataSource.Volunteers.Remove(volunteer);
+    }
+
+    public void DeleteAll()
+    {
+        DataSource.Volunteers.Clear();
     }
 }
