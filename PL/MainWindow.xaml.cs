7b0600ceca68a83aa60fde286a4f28cac73ba5cc
איילת בניסטי <ayelet.benisty@gmail.com>
2024-12-28 23:53:15 +0200

    until 6 6

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PL
 {
@@ -16,9 +9,53 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize Current Time to now
+            CurrentTime = DateTime.Now;
+
+            // Start a timer to update the time every second
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += (s, e) => CurrentTime = DateTime.Now;
+            timer.Start();
+        }
+
+        // Dependency Property for CurrentTime
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+        // Methods for advancing the clock
+        private void AddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.MINUTE);  // Call to advance by one minute
+        }
+
+        private void AddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.HOUR);  // Call to advance by one hour
+        }
+
+        private void AddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.DAY);  // Call to advance by one day
+        }
+
+        private void AddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.YEAR);  // Call to advance by one year
         }
     }
 }
